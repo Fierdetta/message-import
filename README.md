3e87de6abfab8d1a72f9cf656955584b84bb0194
Jack Matthews <jm5112356@gmail.com>
2023-03-07 22:02:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Message import
+Allows you to import plugins directly from a message's action sheet.
---
+++ src/index.tsx
@@ -0,0 +1,77 @@
+import { findByDisplayName, findByProps } from "@vendetta/metro";
+import { React } from "@vendetta/metro/common";
+import { after, before } from "@vendetta/patcher";
+import { installPlugin } from "@vendetta/plugins";
+import { getAssetIDByName } from "@vendetta/ui/assets";
+import { Forms } from "@vendetta/ui/components";
+import { showToast } from "@vendetta/ui/toasts";
+import { safeFetch } from "@vendetta/utils";
+import { APIMessage } from "discord-api-types/v10";
+
+const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
+
+const { FormRow } = Forms;
+const Icon = findByDisplayName("Icon");
+
+const HTTP_REGEX = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+
+let unpatch;
+
+export default {
+    onLoad: () => {
+        unpatch = before("openLazy", LazyActionSheet, ([sheet, name]) => {
+            if (name !== "MessageLongPressActionSheet") return;
+
+            sheet.then((instance) => {
+                const unpatchInstance = after("default", instance, ([{ message }]: [{ message: APIMessage }], res) => {
+                    React.useEffect(() => () => { unpatchInstance() }, []);
+
+                    const [pluginManifests, setPluginManifests] = React.useState<Indexable<PluginManifest>>({});
+
+                    React.useEffect(() => {
+                        const fetchPluginManifests = async () => {
+                            const links = message.content.match(HTTP_REGEX).map((link) => link.endsWith("/") ? link : `${link}/`);
+                            
+                            const pluginManifests: Indexable<PluginManifest> = {};
+
+                            for (const link of links) {
+                                try {
+                                    const pluginManifest = await (await safeFetch(link + "manifest.json", { cache: "no-store" })).json() as PluginManifest;
+                                    pluginManifests[link] = pluginManifest;
+                                } catch {
+                                    continue;
+                                };
+                            };
+
+                            setPluginManifests(pluginManifests);
+                        };
+
+                        fetchPluginManifests();
+                    }, [message.content])
+
+                    let rows = res?.props?.children?.props?.children?.props?.children[1] as Array<JSX.Element>;
+
+                    for (const pluginLink of Object.keys(pluginManifests)) {
+                        const installPluginRow = (<FormRow
+                            leading={<Icon source={getAssetIDByName("ic_download_24px")} />}
+                            label={`Install ${pluginManifests[pluginLink].name}`}
+                            onPress={() => {
+                                // @ts-expect-error
+                                installPlugin(pluginLink).then(() => {
+                                      showToast(`Successfully installed ${pluginManifests[pluginLink].name}`, getAssetIDByName("Check"));
+                                  }).catch((e) => {
+                                    showToast(e.message, getAssetIDByName("Small"));
+                                  })
+                            }}
+                        />);
+    
+                        rows.unshift(installPluginRow);
+                    };
+                });
+            });
+        });
+    },
+    onUnload: () => {
+        unpatch();
+    },
+};
